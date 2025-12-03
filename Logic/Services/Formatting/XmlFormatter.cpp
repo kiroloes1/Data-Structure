b4a90462706ac3ec147b6b9039ab3b14cd114528
Nouran Atef <engineer_nouran@yahoo.com>
2025-12-03 21:53:24 +0200

    Relocate XmlFormatter files to appropriate directory within Services and update include paths.

--- Logic/Services/Formatting/XmlFormatter.cpp
+++ Logic/Services/Formatting/XmlFormatter.cpp
@@ -1,3 +1,25 @@
-//
-// Created by Nouran Atef on 01/12/2025.
-//
+#include <iostream>
+#include <fstream>
+#include "XmlFormatter.h"
+
+using namespace std;
+
+int main() {
+    //Example
+    string xml = R"(
+<social>
+<user><id>1</id><name>Aly</name></user>
+<user><id>2</id><name>Mona</name></user>
+</social>
+)";
+
+    string formatted = prettify_xml(xml);
+
+    cout << formatted << endl;
+
+    ofstream outfile("output.xml");
+    outfile << formatted;
+    outfile.close();
+
+    return 0;
+}
--- Logic/Services/Formatting/XmlFormatter.h
+++ Logic/Services/Formatting/XmlFormatter.h
@@ -1,8 +1,67 @@
-//
-// Created by Nouran Atef on 01/12/2025.
-//
+#ifndef XMLFORMATTER_H
+#define XMLFORMATTER_H
 
-#ifndef DATA_STRUCTURE_XMLFORMATTER_H
-#define DATA_STRUCTURE_XMLFORMATTER_H
+#include <string>
+#include <vector>
+#include <regex>
 
-#endif //DATA_STRUCTURE_XMLFORMATTER_H
+using namespace std;
+
+
+vector<string> tokenize(const string& xml) {
+    vector<string> tokens;
+    regex re("<[^>]+>|[^<]+");
+    sregex_iterator begin(xml.begin(), xml.end(), re);
+    sregex_iterator end;
+
+    for (auto it = begin; it != end; ++it) {
+        string token = it->str();
+        if (token.find_first_not_of(" \t\n\r") != string::npos)
+            tokens.push_back(token);
+    }
+    return tokens;
+}
+
+
+string prettify_xml(const string& xml, const string& indent_unit = "    ") {
+    vector<string> tokens = tokenize(xml);
+    string pretty_xml;
+    int indent_level = 0;
+
+    for (size_t i = 0; i < tokens.size(); ++i) {
+        string token = tokens[i];
+        token = regex_replace(token, regex("^\\s+|\\s+$"), "");
+
+        string next_token = (i + 1 < tokens.size()) ? regex_replace(tokens[i + 1], regex("^\\s+|\\s+$"), "") : "";
+        string next_next_token = (i + 2 < tokens.size()) ? regex_replace(tokens[i + 2], regex("^\\s+|\\s+$"), "") : "";
+
+
+        if (!token.empty() && token[0] == '<' && token[1] != '/' && token.back() != '/' &&
+            !next_token.empty() && next_token[0] != '<' &&
+            !next_next_token.empty() && next_next_token.substr(0, 2) == "</") {
+
+            pretty_xml += string(indent_level * indent_unit.size(), ' ') + token + next_token + next_next_token + "\n";
+            i += 2;
+            continue;
+        }
+
+        if (token.substr(0, 2) == "</") {
+            indent_level--;
+            pretty_xml += string(indent_level * indent_unit.size(), ' ') + token + "\n";
+        }
+        else if (token.back() == '/' && token[0] == '<') {
+            pretty_xml += string(indent_level * indent_unit.size(), ' ') + token + "\n";
+        }
+        else if (token[0] == '<') {
+            pretty_xml += string(indent_level * indent_unit.size(), ' ') + token + "\n";
+            indent_level++;
+        }
+        else {
+            pretty_xml += string(indent_level * indent_unit.size(), ' ') + token + "\n";
+        }
+    }
+
+    return pretty_xml;
+}
+
+#endif
