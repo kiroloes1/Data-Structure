4b1fa0fe4b97f8b9c35dff024504e2824c5258ec
alytamer-01 <alytamer120@gmail.com>
2025-12-04 02:37:13 +0200

    Update XmlFormatter.h

--- Logic/Services/Formatting/XmlFormatter.h
+++ Logic/Services/Formatting/XmlFormatter.h
@@ -3,65 +3,11 @@
 
 #include <string>
 #include <vector>
-#include <regex>
 
 using namespace std;
 
 
-vector<string> tokenize(const string& xml) {
-    vector<string> tokens;
-    regex re("<[^>]+>|[^<]+");
-    sregex_iterator begin(xml.begin(), xml.end(), re);
-    sregex_iterator end;
+vector<string> tokenize(const string& xml);
+string prettify_xml(const string& xml, const string& indent_unit = "    ");
 
-    for (auto it = begin; it != end; ++it) {
-        string token = it->str();
-        if (token.find_first_not_of(" \t\n\r") != string::npos)
-            tokens.push_back(token);
-    }
-    return tokens;
-}
-
-
-string prettify_xml(const string& xml, const string& indent_unit = "    ") {
-    vector<string> tokens = tokenize(xml);
-    string pretty_xml;
-    int indent_level = 0;
-
-    for (size_t i = 0; i < tokens.size(); ++i) {
-        string token = tokens[i];
-        token = regex_replace(token, regex("^\\s+|\\s+$"), "");
-
-        string next_token = (i + 1 < tokens.size()) ? regex_replace(tokens[i + 1], regex("^\\s+|\\s+$"), "") : "";
-        string next_next_token = (i + 2 < tokens.size()) ? regex_replace(tokens[i + 2], regex("^\\s+|\\s+$"), "") : "";
-
-
-        if (!token.empty() && token[0] == '<' && token[1] != '/' && token.back() != '/' &&
-            !next_token.empty() && next_token[0] != '<' &&
-            !next_next_token.empty() && next_next_token.substr(0, 2) == "</") {
-
-            pretty_xml += string(indent_level * indent_unit.size(), ' ') + token + next_token + next_next_token + "\n";
-            i += 2;
-            continue;
-        }
-
-        if (token.substr(0, 2) == "</") {
-            indent_level--;
-            pretty_xml += string(indent_level * indent_unit.size(), ' ') + token + "\n";
-        }
-        else if (token.back() == '/' && token[0] == '<') {
-            pretty_xml += string(indent_level * indent_unit.size(), ' ') + token + "\n";
-        }
-        else if (token[0] == '<') {
-            pretty_xml += string(indent_level * indent_unit.size(), ' ') + token + "\n";
-            indent_level++;
-        }
-        else {
-            pretty_xml += string(indent_level * indent_unit.size(), ' ') + token + "\n";
-        }
-    }
-
-    return pretty_xml;
-}
-
-#endif
+#endif 
