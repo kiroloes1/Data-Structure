e8713b0a9e58745d165348475d0a737278010a30
Nouran Atef <engineer_nouran@yahoo.com>
2025-12-09 23:06:33 +0200

    Remove redundant global XML formatting functions; streamline XmlFormatter class and clean up unused XmlMinifier binary.

--- Logic/Services/Formatting/XmlFormatter.h
+++ Logic/Services/Formatting/XmlFormatter.h
@@ -4,13 +4,6 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
-
-vector<string> tokenize(const string& xml);
-string prettify_xml(const string& xml, const string& indent_unit = "    ");
-
-#endif 
 class XmlFormatter {
 public:
     std::string prettify_xml(const std::string& xml);
