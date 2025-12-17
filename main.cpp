b56e100226abacfee9349488a4d851dd024b9c1f
fatma-hashem <fatmahashem04@gmail.com>
2025-12-17 02:12:47 +0200

    Add graph visualization feature with draw command

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
-//
-// Created by Nouran Atef on 01/12/2025.
-//
 #include <QApplication>
-#include <QPushButton>
-
 #include "GUI/mainwindow.h"
-
-#include <QApplication>
+#include "CLI/CommandLine.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    // Check if running in CLI mode (has command line arguments)
+    if (argc > 1) {
+        // CLI Mode
+        std::cout << "=== CLI Mode Activated ===" << std::endl;
+        std::cout << "Arguments: " << argc << std::endl;
+        for (int i = 0; i < argc; i++) {
+            std::cout << "  argv[" << i << "]: " << argv[i] << std::endl;
+        }
+        std::cout << std::endl;
+
+        CommandLine cli(argc, argv);
+        int result = cli.execute();
+
+        std::cout << "=== CLI Execution Complete (exit code: " << result << ") ===" << std::endl;
+        return result;
+    } else {
+        // GUI Mode
+        std::cout << "=== GUI Mode ===" << std::endl;
+        QApplication a(argc, argv);
+        MainWindow w;
+        w.show();
+        return a.exec();
+    }
 }
